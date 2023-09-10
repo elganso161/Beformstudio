ebfe0e1d56016154642570431ff97ec111e5d59b
el ganso <elganso45@gmail.com>
2023-09-11 01:40:13 +0300

    beform

--- source/js/helper.js
+++ source/js/helper.js
@@ -7,4 +7,10 @@ helperButton.addEventListener("click", () => {
   helperImgOpen.classList.toggle("active");
   helperImgClose.classList.toggle("active");
   helperBody.classList.toggle("active");
+
+  if (helperImgClose.classList.contains("active")) {
+    helperButton.style.animation = "none";
+  } else {
+    helperButton.style.animation = "animate_pulse 2s infinite";
+  }
 });
